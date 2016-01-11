2a47b7dccbd72ae5ec2edaca23d04606da130dd5
Ken Crocken <kcrocken@gmail.com>
2016-01-11 01:45:34 -0500

    Updates to 0.0.3 gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,15 @@ class WelcomeController < ApplicationController
     
     def index
         x = DCMetro::Information.new
-        @alerts = x.alerts['Incidents']
-        @lines = x.line
-        @stations = x.line "green"
-        @college_park = x.station "college"
-        @gallery_place = x.station "gallery"
+        @alerts = JSON.parse(x.alerts)
+        puts @alerts
+        @lines = JSON.parse(x.line)
+        puts "==================== #{@lines['Lines']}"
+        @stations = JSON.parse x.line "green"
+        puts "==================== #{@stations['Stations']}"
+        @college_park = JSON.parse x.station "college"
+        puts "==================== #{@college_park}"
+        @gallery_place = JSON.parse x.station "gallery"
         #
         # Not available in DCMetro 0.0.2
         # @fare_info = x.station "college", "gallery"
